d98a1761d6a733cedfbc9b54dd167fcbb354c301
Alain IVARS <alainivars@gmail.com>
2019-06-28 14:10:58 +0100

    add script terraform for local deployment of a cluster docker-machine

---
+++ terraform/skeleton/local/docker-machine-cluster-3-nodes.tf
@@ -0,0 +1,19 @@
+resource "dockermachine_virtualbox" "node" {
+    count = 3
+    name = "${format("node-%02d", count.index+1)}"
+    virtualbox_cpu_count = 1
+    virtualbox_memory = 1024
+
+    provisioner "remote-exec" {
+        inline = [
+            "touch /tmp/this_is_a_test",
+        ]
+        connection {
+            type        = "ssh"
+            host        = "${self.ssh_hostname}"
+            port        = "${self.ssh_port}"
+            user        = "${self.ssh_username}"
+            private_key = "${file("${self.ssh_keypath}")}"
+        }
+    }
+}
